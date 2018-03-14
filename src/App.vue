dbd8bcfd93557eb7e71815073ca8d4ac42e2d619
Tessa <Tessa>
2018-03-14 16:09:57 +1100

    set up components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
     <div class="container">
-    
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import QuoteGrid from "./components/QuoteGrid.vue";
+export default {
+  data: function() {
+    return {
+      quotes: ["just a quote to see something"],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid
+  }
+};
 </script>
 
 <style>
+
 </style>
